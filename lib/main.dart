24fd7a505049bf68803b612ac3d6ee7b07422b0b
Sokleng <soklengbun22@gmail.com>
2024-01-25 15:59:42 +0700

    add touch detech

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bamboo_shoot/controllers/theme_controller.dart';
+import 'package:bamboo_shoot/controllers/touch_controller.dart';
 import 'package:bamboo_shoot/locales/locales.dart';
 import 'package:bamboo_shoot/views/pages/pages.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() {
+  Get.put(ThemeController());
+  Get.put(TouchController());
   runApp(const MyApp());
 }
 
@@ -18,38 +21,38 @@ class MyApp extends StatelessWidget {
     final ThemeController themeController = Get.put(ThemeController());
 
     return ScreenUtilInit(
-        designSize: const Size(360, 690),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (_, child) {
-          return Obx(
-            () {
-              return GetMaterialApp(
-                defaultTransition: Transition.cupertino,
-                theme:
-                    themeController.themes[themeController.currentTheme.value],
-                debugShowCheckedModeBanner: true,
-                initialRoute: Pages.homeScreen,
-                getPages: Pages.pages,
-                translations: AppTranslations(),
-                supportedLocales: const [
-                  Locale('en', 'US'),
-                  Locale('zh', 'CN'),
-                ],
-                localizationsDelegates: const [
-                  GlobalMaterialLocalizations.delegate,
-                  GlobalWidgetsLocalizations.delegate,
-                  GlobalCupertinoLocalizations.delegate,
-                ],
-                locale: Get.locale,
-                fallbackLocale: const Locale('en', 'US'),
-                scrollBehavior: const ScrollBehavior().copyWith(
-                  overscroll: true,
-                  physics: const BouncingScrollPhysics(),
-                ),
-              );
-            },
-          );
-        });
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return Obx(
+          () {
+            return GetMaterialApp(
+              defaultTransition: Transition.cupertino,
+              theme: themeController.themes[themeController.currentTheme.value],
+              debugShowCheckedModeBanner: true,
+              initialRoute: Pages.homeScreen,
+              getPages: Pages.pages,
+              translations: AppTranslations(),
+              supportedLocales: const [
+                Locale('en', 'US'),
+                Locale('zh', 'CN'),
+              ],
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              locale: Get.locale,
+              fallbackLocale: const Locale('en', 'US'),
+              scrollBehavior: const ScrollBehavior().copyWith(
+                overscroll: true,
+                physics: const BouncingScrollPhysics(),
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
--- lib/views/pages/home/home.dart
+++ lib/views/pages/home/home.dart
@@ -1,21 +1,71 @@
 // import 'package:bamboo_shoot/views/themes/theme.dart';
 import 'package:bamboo_shoot/controllers/theme_controller.dart';
+import 'package:bamboo_shoot/controllers/touch_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   final ThemeController theme = Get.find();
+  final TouchController touch = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomePage'.tr),
-      ),
-      body: TextButton(
-        onPressed: () => theme.changeTheme(),
-        child: Text('change theme'),
+    return Listener(
+      onPointerDown: (details) {
+        touch.showAnimation.value = false;
+        touch.top.value = details.localPosition.dy;
+        touch.left.value = details.localPosition.dx;
+        touch.showAnimation.value = true;
+      },
+      onPointerMove: (details) {
+        touch.top.value = details.localPosition.dy;
+        touch.left.value = details.localPosition.dx;
+      },
+      onPointerUp: (details) async {
+        touch.isAnimationEnd.value = true;
+        await Future.delayed(const Duration(milliseconds: 1000), () {
+          touch.showAnimation.value = false;
+          touch.isAnimationEnd.value = false;
+        });
+      },
+      child: Stack(
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'HomePage'.tr,
+                style: TextStyle(fontSize: 14.sp),
+              ),
+            ),
+            body: Center(
+              child: TextButton(
+                onPressed: () => theme.changeTheme(),
+                child: Text(
+                  'change theme ${touch.top.value}',
+                  style: TextStyle(fontSize: 12.sp),
+                ),
+              ),
+            ),
+          ),
+          Obx(() {
+            return Visibility(
+              visible: touch.showAnimation.value,
+              child: Positioned(
+                top: touch.top.value,
+                left: touch.left.value,
+                child: Image.asset(
+                  touch.isAnimationEnd.value
+                      ? 'assets/image/kokkoro_dance.gif'
+                      : 'assets/image/kokkoro.gif',
+                  height: 20.w,
+                  width: 20.w,
+                ),
+              ),
+            );
+          }),
+        ],
       ),
     );
   }
